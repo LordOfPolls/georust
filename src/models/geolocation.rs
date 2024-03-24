65e322856f91be494057f832eafbf119c8f737b4
LordOfPolls <dev@lordofpolls.com>
2024-03-24 09:15:02 +0000

    feat: add distance method to Geolocation

--- src/models/geolocation.rs
+++ src/models/geolocation.rs
@@ -1,5 +1,14 @@
+use crate::haversine::calculate_distance;
+
 #[derive(Debug, PartialEq, Clone)]
 pub struct GeoLocation {
     pub latitude: f64,
     pub longitude: f64,
+}
+
+
+impl GeoLocation {
+    pub fn distance(&self, other: &GeoLocation) -> f64 {
+        calculate_distance(self, other)
+    }
 }
